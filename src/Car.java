d337f52e04775c21d5337e9739ccf9b4b72e6a5b
nurbek <nurbek07072007@gmail.com>
2023-03-17 20:24:31 +0600

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    String marca;
+    String model;
+    String color;
+    String price;
+    String maxspeed;
+//    public  (){
+//        merseds,toyota,hyundai,honda,waz.
+//    }
+}
